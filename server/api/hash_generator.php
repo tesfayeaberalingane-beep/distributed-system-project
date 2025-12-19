34b9282384d9c39637657732bd5846fe04d1ea07
tesfayeaberalingane-beep <tesfayeaberalingane@gmail.com>
2025-12-19 10:11:45 +0000

    server/api directory

--- server/api/hash_generator.php
+++
@@ -1,7 +0,0 @@
-<?php
-$password = '1344'; // The plaintext password you want to use
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-echo "Plaintext: " . $password . "\n";
-echo "New Hash: " . $hashed_password . "\n";
-?>
